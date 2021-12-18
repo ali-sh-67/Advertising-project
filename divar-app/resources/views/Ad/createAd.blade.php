89f9082f58554bf6c3d74a75d4a61783e5e0feb9
JavadMohammadi <mehrab548@yahoo.com>
2021-12-18 16:31:05 +0330

    chanje file

--- divar-app/resources/views/Ad/createAd.blade.php
+++ divar-app/resources/views/Ad/createAd.blade.php
@@ -16,45 +16,43 @@
 
 @section('content') -->
 
-    <div style="width: 711px;height:550px;border:solid 1px black;margin-right:340px;cursor:pointer;background-color:seashell;">
+    <div id="create_main">
         @foreach($errors->all() as $error)
             {{$error}}
         @endforeach
 
         
-        <div style="width: 515px;height:550px;margin-right:85px; ">
+        <div id="create_form">
             <form  method="post"  action="{{route('storeAd')}}" enctype="multipart/form-data">
                 @csrf
 
-                <div>
-                    <div ><label style="margin-right:52px;">نام آگهی:</label></div>
-                    <div ><input style="width: 430px;margin-right:52px;" type="text" name="title" placeholder="نام آگهی را وارد کنید"></div>
-                </div>
-                <div>
-                    <div><label style="margin-right:52px;">توضیحات:</label></div>
-                    <textarea style="width: 430px;height:160px;margin-right:52px;resize: none;" name="description" placeholder=" توضیحات" ></textarea>
+               
+                    <label class="label_ad" >نام آگهی:</label>
+                    <input class="input_Ad"  type="text" name="title" placeholder="نام آگهی را وارد کنید">
+              
+                
+                    <label id="label_description" >توضیحات:</label>
+                    <textarea id="textarea_description"  name="description" placeholder="توضیحات را وارد کنید" ></textarea>
                     
-                </div>
-                <div>
-                    <div><label style="margin-right:52px;">تصویر:</label></div>
-                    <div><input style="margin-right:52px;"  type="file" name="image_url"></div>
-                </div>
-                <div>
-                    <div><label style="margin-right:52px;">قیمت:</label></div>
-                    <div><input style="width: 430px;margin-right:52px;" type="text" name="price" placeholder="قیمت"></div>
-                </div>
-                <div>
-                    <div><label style="margin-right:52px;">آدرس:</label></div>
-                    <div><input style="width: 430px;margin-right:52px;" type="text" name="address" placeholder="آدرس"></div>
-                </div>
-                <div>
-                    <div><label style="margin-right:52px;">تلفن:</label></div>
-                    <div><input style="width: 430px;margin-right:52px;" type="text" name="phone_number_ads" placeholder="تلفن"></div>
-                </div>
-                <div>
-                    <div style="margin-top: 10px;;">                    
-                    <input style="background-color:rgba(11,194,240,0.932)  ;border-radius:20%;width:100px;margin-right:223px;" type="submit" value="ارسال">
-                </div>
+               
+                
+                    <label id="label_img">تصویر:</label>
+                    <input id="input_img" type="file" name="image_url">
+                
+               
+                    <div><label class="label_ad " >قیمت:</label></div>
+                    <div><input class="input_Ad" type="text" name="price" placeholder="قیمت را وارد کنید"></div>
+              
+                    <div><label class="label_ad" >آدرس:</label></div>
+                    <div><input class="input_Ad" type="text" name="address" placeholder="آدرس را وارد کنید"></div>
+            
+                    <div><label class="label_ad">تلفن:</label></div>
+                    <div><input class="input_Ad" type="text" name="phone_number_ads" placeholder="شماره تلفن را وارد کنید"></div>
+               
+             
+                                       
+                    <input id="input_submit"  type="submit" value="ارسال">
+               
                 </div>
             </form>
         </div>
