56777aa6dc2f41a2a51675da17ff52689e81fdb3
JavadMohammadi <mehrab548@yahoo.com>
2021-12-25 16:55:59 +0330

    edit

--- divar-app/resources/views/Ad/showAd.blade.php
+++ divar-app/resources/views/Ad/showAd.blade.php
@@ -1,21 +1,90 @@
-@extends('layouts.main')
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<title>صفحه اصلی دیوار</title>
+<script src="js/jquery-3.4.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+
+<body class="body" >
 
 
-<div class="d-flex justify-content-center">
-    
-    عنوان آگهی   :{{$id->title}}<br>
-      توضیحات :{{$id->description}}<br>
-      آدرس :{{$id->address}}<br>
-      قیمت :{{$id->price}}<br>
-     تاریخ ایجاد:{{$id->created_at}}<br>
-     تصویر:<img src="{{asset('images/' .$id->image_url)}}">
+
+<div class="main">
+
+  
+
+
+    <div class="create" >
+      <div class="advert" >
+        <span class="nameAdvert">کد آگهی: {{$id->id}} </span>
+      </div>
+
+      <div class="img" >
+        <img class="showImg"  src="{{asset('images/' .$id->image_url)}}">
+      </div>
+
+      <div class="title" >
+        <span >عنوان: {{$id->title}}</span>
+      </div>
+      <div class="description">
+        <span >توضیحات: {{$id->description}}</span>
+      </div>
+      <div id="PriceTell">
+        <div class="price" >
+          <span class="showPrice" >قیمت: <span style="color:red;">{{$id->price }} </span>تومان</span>
+        </div>
+        <span  class="showTell">تلفن: {{$id->phone_number_ads}} </span>
+        
+       
+      </div>
+      <div class="Address" >
+        <span >آدرس: {{$id->address }}</span>
+      </div>
      
-     
 
-</div>
-</div>
+      <div class="comment"  >
+        <span class="comm_span">کامنت:</span>
+        <div class="comm_space">
+          @foreach($comms as $comm)
+                @if($comm->Ads_id == $id->id)
+
+                  @foreach($users as $user)  
+                  @if(($user->id)%2==0)
+                    @if($comm->user_id == $user->id )          
+                      <span class="comm_user" style="color:green;" >{{$user->name}}:</span>
+                    @endif
+                  @endif
+
+                  @if(($user->id)%2==1)
+                    @if($comm->user_id == $user->id )          
+                      <span class="comm_user" style="color:red;" >{{$user->name}}:</span>
+                    @endif
+                  @endif
+
+                  
+                  @endforeach
+                  
+                  {{$comm->description }}
+                  <br>
+                @endif
+          @endforeach
+        
+        </div> 
+       
+                
+              
+      </div> 
+    </div>    
 
 
+ 
+  
+  </div>
+  
 
-@endsection
+</body>
+</html>
