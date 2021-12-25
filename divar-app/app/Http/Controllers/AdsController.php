31024d9a62c3e8d42238c43933848352eb5965da
ali-sh-67 <ali.shahbeaki@gmail.com>
2021-12-25 11:41:34 +0330

    continue section allfavorite

--- divar-app/app/Http/Controllers/AdsController.php
+++ divar-app/app/Http/Controllers/AdsController.php
@@ -12,9 +12,9 @@ use Illuminate\Support\Facades\DB;
 class AdsController extends Controller
 {
     public function indexAd(Request $request)
-    {  
-        $users = DB::table('users')->get();              
-        $id = Auth::user()->id;        
+    {
+        $users = DB::table('users')->get();
+        $id = Auth::user()->id;
         $ads=DB::table('Ads')->orderBy('id','Desc')->paginate(5);
         $comms=comment::all();
         $unames=User::all();
@@ -22,8 +22,8 @@ class AdsController extends Controller
         return view('Ad.pageAd',compact('ads','comms','users','favs'));
     }
 
-    public function myListAd(Request $request){                      
-        $id = Auth::user()->id;        
+    public function myListAd(Request $request){
+        $id = Auth::user()->id;
         $ads=DB::table('Ads')->orderBy('id','Desc')->paginate(10);
         $count=DB::table('Ads')->orderBy('id','Desc')->count();
         return view('Ad.myListAd',compact('ads','count'));
@@ -31,14 +31,14 @@ class AdsController extends Controller
     }
 
    public function createAd()
-   {  
+   {
     $categories = category::all();
-       return view('Ad.createAd')->with(['categories'=> $categories]); 
+       return view('Ad.createAd')->with(['categories'=> $categories]);
    }
 
    public function storeAd(Request $request)
     {
-        $newImageName =time() . '-' . $request->name . '.' . $request->image_url->extension();        
+        $newImageName =time() . '-' . $request->name . '.' . $request->image_url->extension();
         $request->image_url->move(public_path('images'),$newImageName);
 
         $user = Auth::user()->id;
@@ -52,42 +52,42 @@ class AdsController extends Controller
             'address'=>$request->get('address'),
             'phone_number_ads'=> $request->get('phone_number_ads'),
         ]);
-        
-        
-         if ($ad->save()) {            
-            return redirect(route('indexAd'));          
+
+
+         if ($ad->save()) {
+            return redirect(route('indexAd'));
         }
         // return; //422
     }
 
-    public function deleteAd(Ad $id)   
-    {    
+    public function deleteAd(Ad $id)
+    {
         $id->delete();
         return redirect(route('indexAd'));
-    }    
+    }
     public function showAd(Ad $id)
-    {         
-        $users = Auth::user()->name;             
-        return view('Ad.showAd')->with(['id'=>$id]);   
+    {
+        $users = Auth::user()->name;
+        return view('Ad.showAd')->with(['id'=>$id]);
     }
 
     public function editAd(Ad $id)
-    {   
-        $users = Auth::user()->name;  
-        return view('Ad.editAd')->with(['id'=>$id])->with(['users'=>$users]);            
+    {
+        $users = Auth::user()->name;
+        return view('Ad.editAd')->with(['id'=>$id])->with(['users'=>$users]);
     }
     public function updateAd(Request $request, $id)
     {
-        $todo = Ad::where('id', $id)->first();           
+        $todo = Ad::where('id', $id)->first();
         $todo->title= $request->get('title');
         $todo->description= $request->get('description');
-        $todo->price= $request->get('price');         
+        $todo->price= $request->get('price');
 
-         $todo->save();                
-         return redirect(route('indexAd'));   
-    }   
+         $todo->save();
+         return redirect(route('indexAd'));
+    }
     //////////////////////////////////////////////////////////////////////////////////////////
-    
+
     public function favoriteAd (Request $request, $id){
         $user=User::find(Auth::user()->id);
     //    $user->ads()->attach($ad,['favorite'=>'favorite']);
@@ -100,13 +100,13 @@ class AdsController extends Controller
     return view('Ad.pageAd',compact('favs','ads','comms'));
     }
     //////////////////////////////////////////////////////////////////////////////////////////////
-    
+
         public function showfavoriteAd (Request $request){
             // $user=User::find(Auth::user()->id);
     //        $ad=ad::find($id)->id;
     //        $user->ads()->detach($ad,['favorite'=>'favorite']);
     //        $user->ads()->updateExistingPivot($id,['favorite'=> 'not']);
-    
+
             $favs=User::find(Auth::user()->id)->ads()->wherePivot('favorite','favorite')->get();
             return view('Ad.showfavorite',['favs' => $favs]);
         }
@@ -114,8 +114,8 @@ class AdsController extends Controller
 
         public function categoryAds(Request $request, $id){
 
-        $users = DB::table('users')->get();              
-        // $id = Auth::user()->id;        
+        $users = DB::table('users')->get();
+        // $id = Auth::user()->id;
         $ads=DB::table('Ads')->where('category_id',$id)->orderBy('id','Desc')->paginate(5);
         // return $ads;
         $comms=comment::all();
@@ -130,12 +130,13 @@ class AdsController extends Controller
             // for ( $i=1; $i <=$count ; $i++ ){
             //    $allfavs=ad::find(1)->users()->count();
                $allfavs=DB::table('user_ad')->groupBy('ad_id')->selectRaw('count(*) as total,ad_id')
-               ->orderBy('total','desc')->get('ad_id');
+               ->orderBy('total','desc')->get();
+               $allads=ad::all();
             //    foreach($allfavs as $allfav){
                 //    echo $allfav->pivot->user_id;
-                 return view('Ad/allfavoriteAd',compact('allfavs'));
+                 return view('Ad/allfavoriteAd',compact('allfavs','allads'));
             // }
             // }
 
-        }    
+        }
 }
