ee76ce0f7049811dc453be7f6e536299ee3633c8
JavadMohammadi <mehrab548@yahoo.com>
2021-12-27 21:22:00 +0330

    add this link

--- divar-app/resources/views/Ad/pageAd.blade.php
+++ divar-app/resources/views/Ad/pageAd.blade.php
@@ -6,120 +6,131 @@
 <title>صفحه اصلی دیوار</title>
 <script src="js/jquery-3.4.1.min.js"></script>
 <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">                 
+
+                  
+
+
+
+
+
 </head>
 
 <body class="body" >
 
 
-<header class="header" >
-    <div id="myheader" >
-        <div id="header_right">
-            <div id="header_right_top" >
-                <span id="lock" ></span>
+  <header class="header" >
+      <div id="myheader" >
+          <div id="header_right">
+              <div id="header_right_top" >
+                  <span id="lock" ></span>
                   <a class="yekan login" href="">ورود</a>
-                <span id="register"></span>
+                  <span id="register"></span>
                   <a  class="yekan reg" href=""> ثبت نام کنید   </a>
-            </div>
-
-
-            <div id="header_right_bottom" >
-                <div id="search_top">
-                  <input class="yekan input" type="text" placeholder="محصول ، دسته یا برند مورد نظر خود را جستجو کنید ... "/>
-                  <span id="search_pic"></span>
-                 
-                    <form method="get" action="{{route('showfavoriteAd')}}">
+                  <form method="get" action="{{route('showfavoriteAd')}}">
                         @csrf
                         <input type="hidden" name="favorite" value="not"/>
-                        <a onclick="this.parentNode.submit();">showfavorite</a>
-                    </form>
-                 
-                </div>
-<div><a href="{{route('allfavoriteAd')}}">allfavoriteAd</a></div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</header>
-<div class="sidebar">
-
-  @include('category.index')
-
-</div>
-<nav id="nav" ></nav>
-<div class="main">
-
-
-
-    @foreach($ads as $ad)
-
-        <div class="create" >
-          <div class="advert" >
-            <span class="nameAdvert">کد آگهی: {{$ad->id}} </span>
-          </div>
-
-          <div class="img" >
-            <img class="showImg"  src="{{asset('images/' .$ad->image_url)}}">
-          </div>
-
-          <div class="title" >
-            <span >عنوان: {{$ad->title}}</span>
-          </div>
-          <div class="description">
-            <span >توضیحات: {{$ad->description}}</span>
-          </div>
-          <div id="PriceTell">
-            <div class="price" >
-              <span class="showPrice" >قیمت: <span style="color:red;">{{$ad->price }} </span>تومان</span>
-            </div>
-            <span  class="showTell">تلفن: {{$ad->phone_number_ads}} </span>
-            
-          
-          </div>
-          <div class="Address" >
-            <span >آدرس: {{$ad->address }}</span>
-          </div>
-
-
-
-          <div id="comment"  >
-            <span id="comm_span">کامنت:</span>
-            <div id="comm_space">
-                @foreach($comms as $comm)
-                  @if($comm->Ads_id == $ad->id)
-
-                    @foreach($users as $user)
-                    @if(($user->id)%2==0)
-
-                      @if($comm->user_id == $user->id )
-                        <span id="comm_user" style="color:green;" >{{$user->name}}:</span>
-
-                      @endif
-                    @endif
-
-                    @if(($user->id)%2==1)
-
-                      @if($comm->user_id == $user->id )
-                        <span id="comm_user" style="color:red;" >{{$user->name}}:</span>
-
-                      @endif
-                    @endif
-
-
-                    @endforeach
-
-                    {{$comm->description }}
-                    <br>
-                  @endif
-                @endforeach
+                        <a style="margin-right:46px;float:right;" onclick="this.parentNode.submit();">showfavorite</a>
+                    </form> 
+                    <div style="margin-right:54px;float:right;"><a href="{{route('allfavoriteAd')}}">allfavoriteAd</a></div>
               </div>
+
+                    
+                    
+                    
+                  
+
+
+              <div id="header_right_bottom" >                
+                  <div id="search_top">                    
+                    <input class="yekan input" type="text" placeholder="محصول ، دسته یا برند مورد نظر خود را جستجو کنید ... "/>
+                    <span id="search_pic"></span>
+                  </div>
+              </div>
+          </div>       
+      </div>  
+  </header>
+  <nav id="nav" ></nav>
+  <div class="main">
+
+    <div class="sidebar">
+    @include('category.index')
+     
+    </div>
+    
+      @foreach($ads as $ad)
+
+          <div class="create" >
+            <div class="advert" >
+              <span class="nameAdvert">کد آگهی: {{$ad->id}} </span>
             </div>
 
-            <div id="favorite">
+            <div class="img" >
+              <img class="showImg"  src="{{asset('images/' .$ad->image_url)}}">
+            </div>
+
+            <div class="title" >
+              <span >عنوان: {{$ad->title}}</span>
+            </div>
+            <div class="description">
+              <span >توضیحات: {{$ad->description}}</span>
+            </div>
+            <div id="PriceTell">
+              <div class="price" >
+                <span class="showPrice" >قیمت: <span style="color:red;">{{$ad->price }} </span>تومان</span>
+                <div class="showTell">تلفن: {{$ad->phone_number_ads}} </div>
+              </div>
+              
+              
+              
+            
+            </div>
+            <div class="Address" >
+              <span >آدرس: {{$ad->address }}</span>
+            </div>
+          
+
+            <div class="comment"  >
+              <span class="comm_span">کامنت:</span>
+              <div class="comm_space">
+              
+                  @foreach($comms as $comm)
+                    @if($comm->Ads_id == $ad->id)
+
+                      @foreach($users as $user)  
+                      @if(($user->id)%2==0)
+                        @if($comm->user_id == $user->id )          
+                          <span class="comm_user" style="color:green;" >{{$user->name}}:</span>
+                        @endif
+                      @endif
+
+                      @if(($user->id)%2==1)
+                        @if($comm->user_id == $user->id )          
+                          <span class="comm_user" style="color:red;" >{{$user->name}}:</span>
+                        @endif
+                      @endif
+
+                      
+                      @endforeach
+                      
+                      {{$comm->description }}
+                      <br>
+                    @endif
+                  @endforeach
+              </div> 
+            
+              <button class="comm_button" > 
+                <a class="comm_button_a" href="{{route('createComment',$ad->id)}}" >کامنت بگذار!</a>
+              </button>
+
+
+
+
+
+
+
+              <div id="favorite">
+              
               @foreach($favs as $fav)
                 <div id="com_div">
                   @if (empty($fav->pivot))
@@ -137,27 +148,46 @@
                     </form>
                   @endif
                 </div> 
-              @endforeach
-              <form method="post" action="{{route('favoriteAd',['id'=>$ad->id])}}">
-                @csrf
-                <input type="hidden" name="favorite" value="favorite" />
-                <a onclick="this.parentNode.submit();"><i class="fa fa-bookmark-o" style="font-size:48px;color:red"></i></a>
-              </form>
+                  @endforeach
+                  <form method="post" action="{{route('favoriteAd',['id'=>$ad->id])}}">
+                    @csrf
+                    <input type="hidden" name="favorite" value="favorite" />
+                    <a onclick="this.parentNode.submit();"><i class="fa fa-bookmark-o" style="font-size:48px;color:red"></i></a>
+                  </form>
             </div>
-            <button id="comm_button" >
-              <a id="comm_button_a" href="{{route('createComment',$ad->id)}}" >کامنت بگذار!</a>
-            </button>
-          </div>
-        </div>
-    @endforeach
-    <div class="number" >
-      {{ $ads->links() }} 
 
-    </div>
 
+
+
+
+
+              
+              
+
+
+
+
+
+
+                    
+            </div> 
+          </div>    
+
+      @endforeach
+      <div class="number" >
+        {{ $ads->links() }}  
+      </div>
+    
+    
+    
   </div>
-  <div class="nav_footer" > </div>
-  <div class="footer" ></div>
+  <div class="footer">
+    <div class="nav_footer" > </div>
+    <div class="footer_down" ></div>
+  </div>
+  
+ 
 
 </body>
+
 </html>
