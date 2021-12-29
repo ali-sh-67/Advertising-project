27d0974de626fb651c4c72c3bf788554e693b0cb
JavadMohammadi <mehrab548@yahoo.com>
2021-12-30 00:18:52 +0330

    change style

--- divar-app/resources/views/home.blade.php
+++ divar-app/resources/views/home.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="width:1200px;height:511px;border:1px solid black;float:right;margin-right:73px;margin-bottom:25px;background-color:seashell;">
+    <div style="width:1200px;height:511px;border:1px solid black;margin:5px auto;background-color:seashell;">
         <div style="width:200px;height:480px;border:1px solid black;float:right;margin-top:15px;margin-right:15px;background-color:#fff;">
+            <div style="text-align:center;text-decoration: none ;"><a  href="{{route('indexAd')}}">کل آگهی ها</a></div>
             <div style="text-align:center;text-decoration: none ;"><a  href="{{route('createAd')}}">ایجادآگهی</a></div>
             <div style="text-align:center;"><a  href="{{route('myListAd')}}">مدیریت آگهی</a></div>
             <div style="text-align:center;"><a  href="{{route('showfavoriteAd')}}"> مدیریت علاقه مندی</a></div>
