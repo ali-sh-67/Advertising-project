a21ac4247ff9685ea35122bed80acd0f6e43a0bb
JavadMohammadi <mehrab548@yahoo.com>
2021-12-30 00:17:13 +0330

    comment this line

--- divar-app/app/Http/Controllers/Auth/RegisterController.php
+++ divar-app/app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            event(new \App\Events\UserRegister($data['email'])),
+            // event(new \App\Events\UserRegister($data['email'])),
 
         ]);
     }
