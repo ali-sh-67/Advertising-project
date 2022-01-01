2fa05205ca3fd3455f4c7e2e1d5781d4c4a82279
JavadMohammadi <mehrab548@yahoo.com>
2022-01-01 19:21:57 +0330

    edit code

--- divar-app/resources/views/Ad/allfavoriteAd.blade.php
+++ divar-app/resources/views/Ad/allfavoriteAd.blade.php
@@ -1,54 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-
-        <meta charset="UTF-8">
-        <title>all_favorites</title>
-        <script src="js/jquery-3.4.1.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    </head>
-
-    <body class="body">
-
-
-    <header class="header">
-        <div id="myheader">
-            <div id="header_right">
-                <div id="header_right_top">
-                    <span id="lock"></span>
-                    <a class="yekan login" href="">ورود</a>
-                    <span id="register"></span>
-                    <a class="yekan reg" href=""> ثبت نام کنید </a>
-                </div>
-
-
-                <div id="header_right_bottom">
-                    <div id="search_top">
-                        <input class="yekan input" type="text" placeholder="محصول ، دسته یا برند مورد نظر خود را جستجو کنید ... "/>
-                        <span id="search_pic"></span>
-                    </div>
-
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </header>
-    <nav id="nav"></nav>
-    <div id="main">
+@include('layouts.header')
 
         @foreach ($allfavs as $allfav)
             @foreach ($allads as $allad)
@@ -56,38 +6,45 @@
 {{--                    {{$allad->id}}--}}
 {{--                    {{$allad->title}}--}}
 
-            <div id="create">
-                <div id="advert">
-                    <span id="nameAdvert">کد آگهی: {{$allad->id}} تعداد افراد: {{$allfav->total}}</span>
+
+            <div class="createShowFavorid"  >
+                <div class="advert" >
+                    <span class="nameAdvert">کد آگهی: {{$allad->id}} تعداد افراد: {{$allfav->total}}</span>
+                </div>
+                <div class="img" >
+                    <img class="showImgFavorid"  src="{{asset('images/' .$allad->image_url)}}">
                 </div>
 
-                <div id="img">
-{{--                    <img id="showImg" src="{{asset('images/' .$alladd->image_url)}}">--}}
+                <div class="title" >
+                    <span >عنوان: {{$allad->title}}</span>
+                </div>
+                <div class="description">
+                    <span >توضیحات: {{$allad->description}}</span>
+                </div>
+                <div id="PriceTell">
+                    <div class="price" >
+                        <span class="showPrice" >قیمت: <span style="color:red;">{{$allad->price }} </span>تومان</span>
+                        <div class="tell">
+                            <span class="showTell">تلفن: {{$allad->phone_number_ads}} </span>
+                        </div>
+                    </div>    
+                </div>
+                <div class="Address" >
+                    <span >آدرس: {{$allad->address }}</span>
                 </div>
 
-                <div id="title">
-                    <span>عنوان: {{$allad->title}}</span>
+                <div class="comment"  >
+                    <span class="comm_span">کامنت:</span>
+                    <div class="comm_space">
+    
+                    </div>
                 </div>
-                <div id="description">
-                    <span>توضیحات: {{$allad->description}}</span>
-                </div>
-                <div id="price">
-                    <span>قیمت:{{$allad->price }} </span>
-                </div>
-                <div id="Address">
-                    <span>آدرس: {{$allad->address }}</span>
-                </div>
+               
 
+            </div>
+            
 
-                <div id="comment">
-        <span id="com_span">کامنت:
-          <span style="margin-right:53px;margin-top:-30px;display:block;">
-
-          </span>
-
-
-
-        </span>
+       
 
 {{--                    <div id="com_div">--}}
 {{--                        @if (empty($fav->pivot))--}}
@@ -116,16 +73,13 @@
                 @endif
                 @endforeach
                 @endforeach
+                @include('layouts.footer')
         <div id="number">
             {{--        {{ $ads->links() }}--}}
+     
         </div>
+    
 
     </div>
-    <div id="nav_footer"></div>
-    <div id="footer"></div>
-    </div>
-    </body>
-    </html>
+    
 
-</body>
-</html>
