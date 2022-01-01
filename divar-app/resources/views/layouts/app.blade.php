d24065a707fb768661184f0ead4606350ea56967
JavadMohammadi <mehrab548@yahoo.com>
2022-01-01 19:28:36 +0330

    space

--- divar-app/resources/views/layouts/app.blade.php
+++ divar-app/resources/views/layouts/app.blade.php
@@ -61,6 +61,12 @@
                                 {{ Auth::user()->name }}
                             </a>
 
+                           
+
+
+
+
+
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
