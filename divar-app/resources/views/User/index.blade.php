79ab73b66cedba3e1156cd1b3d3ccd37ebd55cc6
JavadMohammadi <mehrab548@yahoo.com>
2022-01-01 19:29:58 +0330

    add code

---
+++ divar-app/resources/views/User/index.blade.php
@@ -0,0 +1,34 @@
+<div style="width:auto;text-align:center;margin-top:105px;background-color:seashell;margin-right:150px;margin-left:150px;"> 
+
+    <div class="container">
+
+        <div class="d-flex justify-content-center">
+            <table class="table">
+                
+                <thead>    
+                    <tr style="color:red;"> 
+                        <td>id</td>           
+                        <td>name</td>           
+                        <td>email</td>                 
+                        <td>status</td>                        
+                    </tr>
+                </thead>
+                <tbody>
+                    @php($i = '1')                    
+                    @foreach($ads as $ad)                   
+                        <tr>           
+                            <td>{{$i}}</td>                  
+                            <td>{{$ad->id}}</td>                  
+                            <td>{{$ad->title}}</td>                  
+                            <td><a href="{{route('editAd',$ad->id)}}">ویرایش</a></td>
+                            <td><a href="{{route('deleteAd',$ad->id)}}"" onclick="return confirm('آیتم مورد نظر حذف شود؟');">حذف</a></td>
+                            <td><a href="{{route('showAd',$ad->id)}}">مشاهده</a></td> 
+                        </tr>
+
+                    @php($i++)
+                    @endforeach                 
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>  
