accab864f09ee422d04b0d8d6bdd55669753bcfc
JavadMohammadi <mehrab548@yahoo.com>
2022-01-01 19:39:11 +0330

    add routs to category

--- divar-app/routes/web.php
+++ divar-app/routes/web.php
@@ -13,25 +13,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/Ad/createAd', [AdsController::class, 'createAd'])->name('createAd')->middleware('auth');
-Route::post('/Ad/storeAd', [AdsController::class, 'storeAd'])->name('storeAd')->middleware('auth');
-Route::get('/Ad/pageAd', [AdsController::class, 'indexAd'])->name('indexAd')->middleware('auth');
-Route::get('/Ad/myListAd', [AdsController::class, 'myListAd'])->name('myListAd')->middleware('auth');
-Route::get('/Ad/showAd/{id}', [AdsController::class, 'showAd'])->name('showAd')->middleware('auth');
-
-Route::get('/Ad/deleteAd/{id}',[AdsController::class, 'deleteAd'])->name('deleteAd')->middleware('auth');
-Route::get('/Ad/editAd/{id}',[AdsController::class, 'editAd'])->name('editAd')->middleware('auth');
-Route::post('/Ad/updateAd/{id}',[AdsController::class, 'updateAd'])->name('updateAd')->middleware('auth');
-
-
-Route::post('/Ad/favoriteAd/{id}',[AdsController::class, 'favoriteAd'])->name('favoriteAd')->middleware('auth');
-Route::post('/Ad/showfavoriteAd',[AdsController::class, 'showfavoriteAd'])->name('showfavoriteAd')->middleware('auth');
-Route::get('/Ad/allfavoriteAd',[AdsController::class, 'allfavoriteAd'])->name('allfavoriteAd')->middleware('auth');
-
+Route::middleware(['auth'])->prefix('/Ad')->group( function () {
+Route::get('/Ad/createAd', [AdsController::class, 'createAd'])->name('createAd');
+Route::post('/Ad/storeAd', [AdsController::class, 'storeAd'])->name('storeAd');
+Route::get('/Ad/pageAd', [AdsController::class, 'indexAd'])->name('indexAd');
+Route::get('/Ad/myListAd', [AdsController::class, 'myListAd'])->name('myListAd');
+Route::get('/Ad/showAd/{id}', [AdsController::class, 'showAd'])->name('showAd');
+Route::get('/Ad/deleteAd/{id}',[AdsController::class, 'deleteAd'])->name('deleteAd');
+Route::get('/Ad/editAd/{id}',[AdsController::class, 'editAd'])->name('editAd');
+Route::post('/Ad/updateAd/{id}',[AdsController::class, 'updateAd'])->name('updateAd');
+Route::post('/Ad/favoriteAd/{id}',[AdsController::class, 'favoriteAd'])->name('favoriteAd');
+Route::post('/Ad/showfavoriteAd',[AdsController::class, 'showfavoriteAd'])->name('showfavoriteAd');
+Route::get('/Ad/allfavoriteAd',[AdsController::class, 'allfavoriteAd'])->name('allfavoriteAd');
 Route::get('/Ad/categoryAds/{id}', [AdsController::class, 'categoryAds'])->name('categoryAds');
 Route::get('/Ad/parentCategoryAds/{id}', [AdsController::class, 'parentCategoryAds'])->name('parentCategoryAds');
-
+});
 
 Route::get('/Comment/create/{id}',[commentController::class, 'createComment'])->name('createComment')->middleware('auth');
 Route::post('/Comment/store/{id}',[commentController::class, 'StoreComment'])->name('StoreComment');
